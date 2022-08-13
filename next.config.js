95ae803177e60cf9d053ec2fbd9e0206e23c61cb
soberdeer <antongraywing@gmail.com>
2022-08-13 14:41:37 +0300

    added prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/rat/' : ''
 }
 
 module.exports = nextConfig
